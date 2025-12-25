9ba3f0a5aa8f118024ee9323ce48879f86e8ebf2
kalyaniii04 <kalyanibj1@gmail.com>
2025-12-25 21:51:13 +0530

    1

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ if (!process.env.ATLAS_URL) {
 
 const express = require("express");
 const app = express();
+app.set("trust proxy", 1);
 const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
